73faa50424ff157d0903298a19a29005f3e37371
KenshinNakamura <k.nakamura@proud-g.jp>
2020-09-24 16:15:31 +0900

    YoutubeController作成

---
+++ server/app/Http/Controllers/api/YoutubeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Google_client;
+use Google_Service_Youtube;
+
+class YoutubeController extends Controller
+{
+    private $googleClient;
+    private $youtube;
+
+    public function __construct()
+    {
+        $this->googleClient = new Google_Client();
+        $this->googleClient->setDeveloperKey(env('GOOGLE_API_KEY'));
+
+        $this->youtube = new Google_Service_Youtube($this->googleClient);
+    }
+
+    public function getLiveStreamingDetails(string $videoId)
+    {
+        $liveStreamingDetails = $this->youtube->videos->listVideos('snippet', [
+            'id' => $videoId,
+            'part' => ['snippet', 'liveStreamingDetails']
+        ]);
+        
+        return $liveStreamingDetails;
+    }
+}
