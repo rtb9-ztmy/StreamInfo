01e146bb61eca0c138821da33827710376eaef37
rtb9 <k.nakamura@proud-g.jp>
2021-07-18 22:11:10 +0900

    API作成

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/search/{id}', 'App\Http\Controllers\api\YoutubeController@getLiveStreamingDetails');
+
+Route::get('twitch/search/{displayName}', 'App\Http\Controllers\api\TwitchController@getLiveStreamingDetails');
